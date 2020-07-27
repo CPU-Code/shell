8552940a5945d6c1ecff3382268bc10b12aedabe
cpucode <923992029@qq.com>
2020-07-27 22:07:59 +0800

    环境变量

---
+++ variable/export.sh
@@ -0,0 +1,17 @@
+###
+ # @Author: cpu_code
+ # @Date: 2020-07-27 21:22:43
+ # @LastEditTime: 2020-07-27 21:23:44
+ # @FilePath: \shell\variable\export.sh
+ # @Gitee: https://gitee.com/cpu_code
+ # @Github: https://github.com/CPU-Code
+ # @CSDN: https://blog.csdn.net/qq_44226094
+ # @Gitbook: https://923992029.gitbook.io/cpucode/
+### 
+#!/bin/bash
+# source ./xxxx.sh
+
+export MYDATA=300
+
+echo MYDATA
+echo $MYDATA
