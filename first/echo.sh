b82c37904f7990fc3f09544be4f39157262e7037
cpucode <923992029@qq.com>
2020-07-27 20:52:26 +0800

    输出信息

---
+++ first/echo.sh
@@ -0,0 +1,20 @@
+###
+ # @Author: cpu_code
+ # @Date: 2020-07-27 19:18:23
+ # @LastEditTime: 2020-07-27 19:53:45
+ # @FilePath: \shell\test.sh
+ # @Gitee: https://gitee.com/cpu_code
+ # @Github: https://github.com/CPU-Code
+ # @CSDN: https://blog.csdn.net/qq_44226094
+ # @Gitbook: https://923992029.gitbook.io/cpucode/
+### 
+# 注释
+#!/bin/bash
+
+# 使用chmod +x xxx.sh 增加权限
+
+echo "cpucode"
+
+#
+# cpucode
+#
