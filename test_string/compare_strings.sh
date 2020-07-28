9923fd4b4a3aa8b0efa1a621164e9969a18c3577
cpucode <923992029@qq.com>
2020-07-28 14:11:43 +0800

    # = 俩个字符串相等 # != 俩个字符串不等 # -z 空串 # -n 非空串

--- test_string/compare_strings.sh
+++ test_string/compare_strings.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: cpu_code
  # @Date: 2020-07-28 14:01:17
- # @LastEditTime: 2020-07-28 14:08:22
+ # @LastEditTime: 2020-07-28 14:11:32
  # @FilePath: \shell\test_string\compare_strings.sh
  # @Gitee: https://gitee.com/cpu_code
  # @Github: https://github.com/CPU-Code
@@ -11,6 +11,11 @@
 #!/bin/bash
 
 read -p "输入一个字符串" str
+
+# = 俩个字符串相等
+# != 俩个字符串不等
+# -z 空串
+# -n 非空串
 
 # 运算符左右必须为空格
 test $str = "cpucode"
