692e682190cf5e4564a40db09e2db4bafced4572
cpucode <923992029@qq.com>
2020-07-30 20:15:08 +0800

    纠错 ` 添加运行结果

--- for/file_attributes.sh
+++ for/file_attributes.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: cpu_code
  # @Date: 2020-07-29 08:31:15
- # @LastEditTime: 2020-07-29 15:59:14
+ # @LastEditTime: 2020-07-30 19:47:22
  # @FilePath: \shell\for\file_attributes.sh
  # @Gitee: https://gitee.com/cpu_code
  # @Github: https://github.com/CPU-Code
@@ -12,13 +12,15 @@
 
 echo "判断当前目录下文件属性"
 
-for name in 'ls'
+for name in `ls`
 do
     #判断
     if [ -d $name ]; then
         echo "$name 是文件夹"
+
     elif [ -f $name ]; then
-        ehco "$name 是文件"
+        echo "$name 是文件"
+        
     else
         echo "$name 是其他文件"
     fi
@@ -27,7 +29,14 @@ done
 
 echo "name = $name"
 
-# cpucode@ubuntu:~/code$ source ./file_.sh 
+# cpucode@ubuntu:~/cpucode/code/shell$ ./file_attributes.sh 
 # 判断当前目录下文件属性
-# ls 是其他文件
-# name = ls
+# case.sh 是文件
+# checkdirall.sh 是文件
+# read.sh 是文件
+# renameall.sh 是文件
+# test 是文件夹
+# Tetris-game.sh 是文件
+# variable_exists.sh 是文件
+# name = variable_exists.sh
+
