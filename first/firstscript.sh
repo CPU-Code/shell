4737c9c9a95aa2800e955fa4f8acb5115063c458
cpucode <923992029@qq.com>
2020-07-30 18:13:58 +0800

    firstscript__环境参数输出

---
+++ first/firstscript.sh
@@ -0,0 +1,22 @@
+###
+ # @Author: cpu_code
+ # @Date: 2020-07-30 12:14:29
+ # @LastEditTime: 2020-07-30 17:41:49
+ # @FilePath: \shell\first\firstscript.sh
+ # @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ # @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ # @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ # @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+### 
+#!/bin/bash
+
+echo "You are welcome to use bash"
+
+echo "Current work dirctory is $PWD"
+echo "You are  $LOGNAME"
+
+ls
+
+# Current work dirctory is /home/cpucode/cpucode/code/shell
+# You are  cpucode
+# case.sh  declare.sh  firstscript.sh
