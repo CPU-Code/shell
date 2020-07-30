bcee4b7baa1ae55f6c273542cea1f3eca548c21f
cpucode <923992029@qq.com>
2020-07-30 20:15:35 +0800

    纠错 ` 添加运行结果

--- variable/variable_exists.sh
+++ variable/variable_exists.sh
@@ -1,3 +1,13 @@
+###
+ # @Author: cpu_code
+ # @Date: 2020-07-27 21:42:56
+ # @LastEditTime: 2020-07-30 19:25:34
+ # @FilePath: \shell\variable\variable_exists.sh
+ # @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ # @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ # @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ # @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+### 
 #!/bin/bash
 
 #${var:-word}	#如果变量var不存在, 表达式的值=word 否则=var
@@ -14,3 +24,9 @@ echo ${num:-100}
 echo ${data:=200}
 
 echo "data = $data"
+
+# cpucode@ubuntu:~/cpucode/code/shell$ ./variable_exists.sh 
+# 100
+# 10
+# 200
+# data = 200
