ba6fe3eeafdf5adfa4bfb872b6e5cf5f49b988d0
cpucode <923992029@qq.com>
2020-07-30 18:09:11 +0800

    function__调用

---
+++ function/function.sh
@@ -0,0 +1,27 @@
+###
+ # @Author: cpu_code
+ # @Date: 2020-07-30 17:29:16
+ # @LastEditTime: 2020-07-30 17:48:47
+ # @FilePath: \shell\function\function.sh
+ # @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ # @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ # @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ # @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+### 
+#!/bin/sh
+
+checkdir()
+{
+	echo "This is checkdir function"
+}
+
+echo "start checking"
+
+checkdir
+
+echo "End"
+
+# cpucode@ubuntu:~/cpucode/code/shell$ ./function.sh 
+# start checking
+# This is checkdir function
+# End
